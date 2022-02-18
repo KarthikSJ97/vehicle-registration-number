f390f873f542fcfd83605fad9f4d3ba168698be6
KarthikSJ97 <karthik.s.jhingade@gmail.com>
2022-02-18 21:49:58 +0530

    [Intial commit] | feature | Initial setup application

---
+++ src/main/java/com/demo/vehicleregistrationnumber/VehicleRegistrationNumberApplication.java
@@ -0,0 +1,13 @@
+package com.demo.vehicleregistrationnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegistrationNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/vehicleregistrationnumber/VehicleRegistrationNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.vehicleregistrationnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
